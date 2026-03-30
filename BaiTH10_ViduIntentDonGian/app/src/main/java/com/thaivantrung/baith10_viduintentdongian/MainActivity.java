1a1a33423ccef75a37c05a76ffb08102ee523459
Văn Trung <162585208+azullshin1109@users.noreply.github.com>
2026-03-30 21:12:07 +0700

    BaiTH10_ViduIntentDonGian

---
+++ BaiTH10_ViduIntentDonGian/app/src/main/java/com/thaivantrung/baith10_viduintentdongian/MainActivity.java
@@ -0,0 +1,37 @@
+package com.thaivantrung.baith10_viduintentdongian;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    //Hàm đáp ứng sk nhấn lên nút "Sang màn hình khác"
+    //Xử lý chuyển màn hình
+    public void ChuyenManHinh(View view){
+        //Tạo một đối tượng Intent
+        //Tham số thứ 2 của hàm tạo này là tên Activity ta muốn chuyển sang
+        Intent iManHinhKhac = new Intent(this, SubActivity.class);
+
+        //Thực hiện chuyển
+        startActivity(iManHinhKhac);
+    }
+}
